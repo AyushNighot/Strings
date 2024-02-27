962d896be2a4c9b0f5dfa89615a8174d15bdabf2
Ayush Nighot <68859871+AyushNighot@users.noreply.github.com>
2024-02-27 10:18:02 +0530

    Check If sentence is PanaGram.cpp

---
+++ Check If sentence is PanaGram.cpp
@@ -0,0 +1,44 @@
+--------------------------------------------------- APPROACH 1-------------------------------------------------------------------------
+
+  class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int>arr(26,0);
+
+        for (auto &ch : sentence)
+        {
+            int index = ch - 'a';
+            arr[index]++;
+        }
+
+        for (auto &count : arr)
+        {
+            if (count == 0) return false;
+        }
+
+        return true;
+    }
+};
+
+------------------------------------------------APPROACH  2 ---------------------------------------------------------------------------
+
+  class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int>arr(26,0);
+        int count = 0;
+        for (auto &ch : sentence)
+        {
+            int index = ch - 'a';
+
+            if(arr[index] == 0)
+            {
+                arr[index]++;
+                count++;
+            }
+        }
+
+        if (count == 26) return true;
+        else   return false;
+    }
+};
