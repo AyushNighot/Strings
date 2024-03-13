9562b6b3422e6d2f96f92c72ac1069887b0dc4d6
Ayush Nighot <68859871+AyushNighot@users.noreply.github.com>
2024-03-13 10:10:33 +0530

    Faulty Keyboard.cpp

---
+++ Faulty Keyboard.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string finalString(string s) {
+        int  n = s.length();
+        string ans = "";
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(s[i] != 'i')
+            {
+                ans.push_back(s[i]);
+            }
+            else 
+            {
+                reverse(ans.begin(),ans.end());
+            }
+        }
+        return ans;
+    }
+};
